c34d5ee0c72592639814480a434c00ac19ec1b3c
Bohdan <Protas>
2020-01-23 12:07:18 +0200

    Setup project

---
+++ DataDrivenTable/Core/Configurator.swift
@@ -0,0 +1,9 @@
+//
+//  Configurator.swift
+//  DataDrivenTable
+//
+//  Created by protas on 1/23/20.
+//  Copyright © 2020 protas. All rights reserved.
+//
+
+import Foundation
---
+++ DataDrivenTable/Core/DataSource.swift
@@ -0,0 +1,9 @@
+//
+//  DataSource.swift
+//  DataDrivenTable
+//
+//  Created by protas on 1/23/20.
+//  Copyright © 2020 protas. All rights reserved.
+//
+
+import Foundation
